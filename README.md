b834e4cba4d579ddd200abf170168ed48fe77b4d
cander <cander_liubiao@126.com>
2021-04-23 15:21:32 +0800

    add delete

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 
 ### 用法
 ```
--m  模块名  content 和 name   content 是修改内容， name 是修改文件名
+-m  模块名  content 和 name   content 是修改内容， name 是修改文件名. delete  删除文件
 -f  文件名， 只有修改内容才有
 -d  目录
 -o  旧字符串
@@ -12,7 +12,9 @@
 -i  指定包含字符串的文件名，逗号分隔多个, 修改内容才有效
 -e   跳过指定包含字符串的文件名，逗号分隔多个， 修改内容才有效
 // 将文件夹 /data/ 下面所有文件内容里面的111替换成222
-go run main.go -m content -d /data/ -o 111 -n 222
+go run fp.go -m content -d /data/ -o 111 -n 222
 // 将文件夹 /data/ 下面所有文件名111替换成222
-go run main.go -m name -d /data/ -o 111 -n 222
+go run fp.go -m name -d /data/ -o 111 -n 222
+// 删除超过1天的txt文件
+go run fp.go -m delete -d /data/ -i txt -t 1
 ```
--- ft.go
+++ ft.go
@@ -9,6 +9,7 @@ import (
 	"os"
 	"path/filepath"
 	"strings"
+	"time"
 )
 
 // var MODULE = []string{"cont", "name"}
@@ -17,15 +18,19 @@ var dir, oldstr, newstr, file_path, include, exclude, module string
 var includeList []string
 var excludeList []string
 var hidden bool
+var delete bool
+var mtime int
 
 func main() {
 	log.Fatal(1)
-	flag.StringVar(&module, "m", "", "文件路径")
+	flag.StringVar(&module, "m", "", "是修改内容还是文件名 content | name | delete")
 	flag.StringVar(&file_path, "f", "", "修改内容才支持文件路径")
 	flag.StringVar(&dir, "d", "", "文件路径")
 	flag.StringVar(&oldstr, "o", "", "原字符串")
 	flag.StringVar(&newstr, "n", "", "新字符串")
 	flag.BoolVar(&hidden, "H", true, "忽略隐藏文件")
+	flag.BoolVar(&delete, "R", false, "删除目录")
+	flag.IntVar(&mtime, "t", 0, "选择时间")
 	flag.StringVar(&include, "i", "", "指定包含字符串的文件名，逗号分隔多个, 修改内容才有效")
 	flag.StringVar(&exclude, "e", "", "跳过指定包含字符串的文件名，逗号分隔多个， 修改内容才有效")
 	flag.Parse()
@@ -35,37 +40,36 @@ func main() {
 	if exclude != "" {
 		excludeList = strings.Split(exclude, ",")
 	}
-	if module == "content" {
+
+	if dir == "" && file_path == "" {
+		log.Fatal("not specify file_path or path")
+	}
+	if dir != "" {
+		if !filepath.IsAbs(dir) {
+			pre_path, _ := filepath.Abs(".")
+			dir = filepath.Join(pre_path, dir)
+		}
+	}
+	switch module {
+	case "content":
 		// 修改内容
-		if dir != "" {
-			if !filepath.IsAbs(dir) {
-				pre_path, _ := filepath.Abs(".")
-				dir = filepath.Join(pre_path, dir)
-			}
-			file_dir(dir)
-		} else if file_path != "" {
+		if file_path != "" {
 			replace(file_path)
-		} else {
-			log.Fatal("not specify file_path or path")
 		}
-	} else if module == "name" {
-		// 修改文件名
 		if dir != "" {
-			if !filepath.IsAbs(dir) {
-				pre_path, _ := filepath.Abs(".")
-				dir = filepath.Join(pre_path, dir)
-			}
-			walkdir(dir, oldstr, newstr)
-		} else {
-			log.Fatal("not specify dir")
+			fileDir(dir)
 		}
-	} else {
+	case "name":
+		walkdir(dir, oldstr, newstr)
+	case "delete":
+		walkdir(dir, oldstr, newstr)
+	default:
 		fmt.Println("module must be content or name")
 	}
 
 }
 
-func file_dir(dirpath string) {
+func fileDir(dirpath string) {
 	infos, err := ioutil.ReadDir(dirpath)
 	if err != nil {
 		log.Fatal(err)
@@ -75,7 +79,7 @@ func file_dir(dirpath string) {
 			continue
 		}
 		if info.IsDir() {
-			file_dir(filepath.Join(dirpath, info.Name()))
+			fileDir(filepath.Join(dirpath, info.Name()))
 		} else {
 			if include == "" && exclude == "" {
 				replace(filepath.Join(dirpath, info.Name()))
@@ -125,6 +129,32 @@ func walkdir(thisdir string, old, new string) {
 				os.Rename(filepath.Join(thisdir, fi.Name()),
 					filepath.Join(thisdir, strings.Replace(fi.Name(), old, new, -1)))
 			}
+		}
+	}
+}
+
+func walkDirDelete(thisdir string) {
+	fl, err := ioutil.ReadDir(thisdir)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println("dir:", thisdir)
+	for _, fi := range fl {
+		if fi.IsDir() {
+			walkDirDelete(filepath.Join(thisdir, fi.Name()))
+		} else {
+			if len(include) > 0 {
+				if strInArray(fi.Name(), includeList) {
+					if mtime > 0 && time.Since(fi.ModTime()) < time.Hour*24*time.Duration(mtime) {
+						// 没超过天数就跳过
+						continue
+					}
+				} else {
+					// 不包含直接跳过
+					continue
+				}
+			}
+			os.RemoveAll(filepath.Join(thisdir, fi.Name()))
 		}
 	}
 }
